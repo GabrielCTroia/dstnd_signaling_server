eb7eb1d37e2a191a6bc1130bdf791cf86b151005
Gabriel C. Troia <catalin.troia@gmail.com>
2020-04-09 18:12:23 +0300

    Add ability to send json to the client

--- src/socket/main.rs
+++ src/socket/main.rs
@@ -1,114 +1,133 @@
 use ws::{
-  listen,
-  Sender,
-  Result,
-  Response,
-  Request,
-  Error,
-  CloseCode,
-  Handshake,
-  Message,
-  Handler,
+    listen, CloseCode, Error, Handler, Handshake, Message, Request, Response, Result,
+    Sender,
 };
 
 use std::cell::Cell;
 use std::rc::Rc;
 
+use serde::{Deserialize, Serialize};
+
 // Server web application handler
 struct Server {
-  out: Sender,
-  count: Rc<Cell<u32>>,
+    out: Sender,
+    count: Rc<Cell<u32>>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ConnectionOpenedPayload {
+    new_connection: String,
+    connections_count: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ConnectionClosedPayload {
+    connections_count: u32,
 }
 
 impl Handler for Server {
-  fn on_request(&mut self, req: &Request) -> Result<(Response)> {
-    match req.resource() {
-        "/ws" => {
-            // used once for const socket = new WebSocket("ws://" + window.location.host + "/ws");
-            // https://blog.stanko.io/do-you-really-need-websockets-343aed40aa9b
-            // and no need for reconnect later
+    fn on_request(&mut self, req: &Request) -> Result<(Response)> {
+        match req.resource() {
+            "/ws" => {
+                // used once for const socket = new WebSocket("ws://" + window.location.host + "/ws");
+                // https://blog.stanko.io/do-you-really-need-websockets-343aed40aa9b
+                // and no need for reconnect later
 
-            // https://ws-rs.org/api_docs/ws/struct.Request.html
-            println!("Browser Request from {:?}", req.origin().unwrap().unwrap());
-            println!("Client found is {:?}", req.client_addr().unwrap());
+                // https://ws-rs.org/api_docs/ws/struct.Request.html
+                println!("Browser Request from {:?}", req.origin().unwrap().unwrap());
+                println!("Client found is {:?}", req.client_addr().unwrap());
 
-            let resp = Response::from_request(req);
-            // println!("{:?} \n", &resp);
-            resp
+                let resp = Response::from_request(req);
+                // println!("{:?} \n", &resp);
+                resp
+            }
+
+            _ => Ok(Response::new(404, "Not Found", b"404 - Not Found".to_vec())),
+        }
+    }
+
+    fn on_open(&mut self, handshake: Handshake) -> Result<()> {
+        // We have a new connection, so we increment the connection counter
+        self.count.set(self.count.get() + 1);
+        let number_of_connection = self.count.get();
+
+        if number_of_connection > 5 {
+            // panic!("There are more user connection than expected.");
         }
 
-        _ => Ok(Response::new(404, "Not Found", b"404 - Not Found".to_vec())),
+        let peer_id = &handshake.peer_addr.unwrap().to_string();
+
+        let payload = ConnectionOpenedPayload {
+            new_connection: peer_id.to_string(),
+            connections_count: number_of_connection,
+        };
+        let serialized = serde_json::to_string(&payload).unwrap();
+
+        println!("{}", &serialized);
+        self.out.broadcast(serialized);
+
+        Ok(())
     }
-  }
 
-  fn on_open(&mut self, handshake: Handshake) -> Result<()> {
-      // We have a new connection, so we increment the connection counter
-      self.count.set(self.count.get() + 1);
-      let number_of_connection = self.count.get();
+    // Handle messages recieved in the websocket (in this case, only on /ws)
+    fn on_message(&mut self, message: Message) -> Result<()> {
+        let raw_message = message.into_text()?;
+        println!("The message from the client is {:#?}", &raw_message);
 
-      if number_of_connection > 5 {
-          // panic!("There are more user connection than expected.");
-      }
-      
-      // The most important part and used to assign id for clients
-      // println!("{}", &handshake.local_addr.unwrap());
-      let open_message = format!("{} entered and the number of live connections is {}", &handshake.peer_addr.unwrap(), &number_of_connection);
+        let message = if raw_message.contains("!warn") {
+            let warn_message = "One of the clients sent warning to the server.";
+            println!("{}", &warn_message);
+            Message::Text("There was warning from another user.".to_string())
+        } else {
+            Message::Text(raw_message)
+        };
 
-      println!("{}", &open_message);
-      self.out.broadcast(open_message);
+        // Broadcast to all connections
+        self.out.broadcast(message)
+    }
 
-      Ok(())
-  }
+    fn on_close(&mut self, code: CloseCode, reason: &str) {
+        match code {
+            CloseCode::Normal => println!("The client is done with the connection."),
+            CloseCode::Away => println!("The client is leaving the site."),
+            CloseCode::Abnormal => {
+                println!("Closing handshake failed! Unable to obtain closing status from client.")
+            }
+            _ => println!("The client encountered an error: {}", reason),
+        }
+        self.count.set(self.count.get() - 1);
 
-  // Handle messages recieved in the websocket (in this case, only on /ws)
-  fn on_message(&mut self, message: Message) -> Result<()> {
-      let raw_message = message.into_text()?;
-      println!("The message from the client is {:#?}", &raw_message);
+        // Once a connecton closes broadcast it
+        let number_of_connection = self.count.get();
 
-      let message = if raw_message.contains("!warn") {
-          let warn_message = "One of the clients sent warning to the server.";
-          println!("{}", &warn_message);
-          Message::Text("There was warning from another user.".to_string())
-      } else {
-          Message::Text(raw_message)
-      };
+        let payload = ConnectionClosedPayload {
+            connections_count: number_of_connection,
+        };
+        let serialized = serde_json::to_string(&payload).unwrap();
 
-      // Broadcast to all connections
-      self.out.broadcast(message)
-  }
+        println!("{}", &serialized);
+        self.out.broadcast(serialized);
+    }
 
-  fn on_close(&mut self, code: CloseCode, reason: &str) {
-      match code {
-          CloseCode::Normal => println!("The client is done with the connection."),
-          CloseCode::Away => println!("The client is leaving the site."),
-          CloseCode::Abnormal => {
-              println!("Closing handshake failed! Unable to obtain closing status from client.")
-          },
-          _ => println!("The client encountered an error: {}", reason),
-      }
-      self.count.set(self.count.get() - 1);
-
-      // Once a connecton closes broadcast it
-      let number_of_connection = self.count.get();
-      let open_message = format!("client closed connection and the number of live connections is {}", &number_of_connection);
-      self.out.broadcast(open_message);
-  }
-
-  fn on_error(&mut self, err: Error) {
-      println!("The server encountered an error: {:?}", err);
-  }
+    fn on_error(&mut self, err: Error) {
+        println!("The server encountered an error: {:?}", err);
+    }
 }
 
 pub fn websocket() -> () {
-  println!("Web Socket Server is ready at ws://127.0.0.1:7777/ws");
-  println!("Server is ready at http://127.0.0.1:7777/");
+    println!("Web Socket Server is ready at ws://127.0.0.1:7777/ws");
+    println!("Server is ready at http://127.0.0.1:7777/");
 
-  // Rc is a reference-counted box for sharing the count between handlers
-  // since each handler needs to own its contents.
-  // Cell gives us interior mutability so we can increment
-  // or decrement the count between handlers.
+    // Rc is a reference-counted box for sharing the count between handlers
+    // since each handler needs to own its contents.
+    // Cell gives us interior mutability so we can increment
+    // or decrement the count between handlers.
 
-  // Listen on an address and call the closure for each connection
-  let count = Rc::new(Cell::new(0));
-  listen("127.0.0.1:7777", |out| { Server { out: out, count: count.clone(), } }).unwrap()
+    // Listen on an address and call the closure for each connection
+    let count = Rc::new(Cell::new(0));
+    listen("127.0.0.1:7777", |out| Server {
+        out: out,
+        count: count.clone(),
+    })
+    .unwrap()
 }
