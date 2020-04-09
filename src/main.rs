b20c4f2804785f0834cb054655e63cee3b433fbe
Gabriel C. Troia <catalin.troia@gmail.com>
2020-04-09 16:15:02 +0300

    Add the websocket logic taken from:
    https://www.steadylearner.com/blog/read/How-to-start-Rust-Chat-App

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+#![feature(
+    proc_macro_hygiene, 
+    decl_macro
+)]
 
 #[macro_use] extern crate rocket;
+
+use std::thread;
+
+extern crate ws;
+
+mod socket;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -8,5 +17,13 @@ fn index() -> &'static str {
 }
 
 fn main() {
+    thread::Builder::new()
+        .name("Thread for Rust Chat with ws-rs".into())
+        // .stack_size(83886 * 1024) // 80mib in killobytes
+        .spawn(|| {
+            socket::main::websocket();
+        })
+        .unwrap();
+
     rocket::ignite().mount("/", routes![index]).launch();
 }
